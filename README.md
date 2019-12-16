e3b3704330df6fe09216fb5767995c907d5bfca1
Edgar Basto <edgar.pt@gmail.com>
2019-12-16 15:17:12 +0000

    training

---
+++ README.md
@@ -0,0 +1,4 @@
+Training from Microsoft Cognitive Services.
+
+https://docs.microsoft.com/en-us/azure/cognitive-services/custom-vision-service/python-tutorial
+
---
+++ training.py
@@ -0,0 +1,79 @@
+from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
+from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateEntry
+
+ENDPOINT = "https://<region>.api.cognitive.microsoft.com/"
+
+# Replace with a valid key
+training_key = "<training key>"
+prediction_key = "<prediction key>"
+prediction_resource_id = "<prediction resource id>"
+
+publish_iteration_name = "classifyModel"
+
+trainer = CustomVisionTrainingClient(training_key, endpoint=ENDPOINT)
+
+# Create a new project
+print ("Creating project...")
+project = trainer.create_project("My New Project")
+print(f'Project name: {project.name}')
+
+# Make two tags in the new project
+hemlock_tag = trainer.create_tag(project.id, "Hemlock")
+cherry_tag = trainer.create_tag(project.id, "Japanese Cherry")
+
+
+base_image_url = "<base dir to images/>"
+
+print("Adding images...")
+
+image_list = []
+
+for image_num in range(1, 11):
+    file_name = "hemlock_{}.jpg".format(image_num)
+    print(f'adding {file_name}')
+    with open(base_image_url + "images/Hemlock/" + file_name, "rb") as image_contents:
+        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[hemlock_tag.id]))
+
+for image_num in range(1, 11):
+    file_name = "japanese_cherry_{}.jpg".format(image_num)
+    print(f'adding {file_name}')
+    with open(base_image_url + "images/Japanese Cherry/" + file_name, "rb") as image_contents:
+        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[cherry_tag.id]))
+
+upload_result = trainer.create_images_from_files(project.id, images=image_list)
+if not upload_result.is_batch_successful:
+    print("Image batch upload failed.")
+    for image in upload_result.images:
+        print("Image status: ", image.status)
+    exit(-1)
+
+if upload_result.is_batch_successful:
+    print('Image batch update sucessful')
+
+
+import time
+
+print ("Training...")
+iteration = trainer.train_project(project.id)
+while (iteration.status != "Completed"):
+    iteration = trainer.get_iteration(project.id, iteration.id)
+    print ("Training status: " + iteration.status)
+    time.sleep(1)
+
+# The iteration is now trained. Publish it to the project endpoint
+trainer.publish_iteration(project.id, iteration.id, publish_iteration_name, prediction_resource_id)
+print ("Done!")
+
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+
+# Now there is a trained endpoint that can be used to make a prediction
+predictor = CustomVisionPredictionClient(prediction_key, endpoint=ENDPOINT)
+
+with open(base_image_url + "images/Test/test_image.jpg", "rb") as image_contents:
+    results = predictor.classify_image(
+        project.id, publish_iteration_name, image_contents.read())
+
+    # Display the results.
+    for prediction in results.predictions:
+        print("\t" + prediction.tag_name +
+              ": {0:.2f}%".format(prediction.probability * 100))
